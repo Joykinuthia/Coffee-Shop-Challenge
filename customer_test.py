3b7352f6699d6a395e4297aa4674a5832d85e00f
Joykinuthia <joyrosekinuthia@gmail.com>
2025-05-17 17:11:42 +0300

    test: add tests for Customer Relationships

--- customer_test.py
+++ customer_test.py
@@ -10,3 +10,14 @@ def test_customer_name_validation():
         Customer("A" * 16)
     customer = Customer("Joy")
     assert customer.name =="Joy"
+
+def test_customer_orders_and_coffees():
+    customer = Customer("Liam")
+    coffee1 = Coffee("Americano")
+    coffee2 = Coffee("Cortado")
+    order1 = Order(customer, coffee1, 5.0)
+    order2 = Order(customer, coffee2, 3.0)
+    assert len(customer.orders()) == 2
+    assert len(customer.coffees()) == 2
+    assert coffee1 in customer.coffees()
+    assert coffee2 in customer.coffees()
