967848f3abe45a36942b0e86eb821c5b8c69da8f
Joykinuthia <joyrosekinuthia@gmail.com>
2025-05-17 16:07:32 +0300

    feat: add class Order self

--- order.py
+++ order.py
@@ -6,3 +6,9 @@ class Order:
             raise TypeError("Customer must be a Customer instance")
         if not isinstance(coffee, Coffee):
             raise TypeError("Coffee must be a Coffee instance")
+        self._customer = customer
+        self._coffee = coffee
+        self._price = None
+        self.price = price
+        customer._orders.append(self)
+        coffee._orders.append(self)
