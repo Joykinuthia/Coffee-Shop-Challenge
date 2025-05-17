6aa491ad2a55eaf5aec8dc717fba5c4e404b4a44
Joykinuthia <joyrosekinuthia@gmail.com>
2025-05-17 16:02:35 +0300

    feat: add Customer instance

--- order.py
+++ order.py
@@ -1,3 +1,6 @@
 class Order:
     def __init__(self, customer, coffee, price):
-        
+        from customer import Customer
+        from coffee import Coffee
+        if not isinstance(customer, Customer):
+            raise TypeError("Customer must be a Customer instance")
