ff9e64d257140da7f08a93a7e51f72f4948fb8e0
Joykinuthia <joyrosekinuthia@gmail.com>
2025-05-17 13:47:59 +0300

    feat: @setter.name str type and character len

--- customer.py
+++ customer.py
@@ -5,3 +5,12 @@ class Customer:
     @property
     def name(self):
         return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+        if not 1 <= len(value) <= 15:
+            raise ValueError("Name must be between 1 and 15 characters")
+        self._name = value
+        
