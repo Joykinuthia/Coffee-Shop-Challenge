b200c87868249e37b84c91b2689171bdefe8e7e4
Joykinuthia <joyrosekinuthia@gmail.com>
2025-05-17 14:30:21 +0300

    feat: @name.setter Coffee class name is immutable

--- coffee.py
+++ coffee.py
@@ -6,4 +6,14 @@ class Coffee:
     @property
     def name(self):
         return self._name
+    
+    @name.setter
+    def name(self, value):
+        if hasattr(self, "_name") and self._name is not None:
+            raise AttributeError("Coffee name is immutable")
+        if not isinstance(value,str):
+            raise TypeError("Name must be a string")
+        if len(value) <3:
+            raise ValueError("Name must be at least 3 characters")
+        self._name = value
         
